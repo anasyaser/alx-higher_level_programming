944bcd985c5acf7998f4895cec172deae1b7f1d6
anasyaser <anoos649@gmail.com>
2023-06-08 13:24:36 +0300

    Fix style issues

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 from sys import argv
 
+
 def print_args():
     argv_c = argv[1:]
     n = len(argv_c)
@@ -13,6 +14,7 @@ def print_args():
 
     for i, arg in enumerate(argv_c):
         print("{}: {}".format(i + 1, arg))
+
 
 if __name__ == "__main__":
     print_args()
