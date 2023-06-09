a37a95172c3443ef39687b40945bb83450e99d87
anasyaser <anoos649@gmail.com>
2023-06-09 15:21:34 +0000

    Modify style errors

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -3,10 +3,3 @@ def delete_at(my_lst=[], idx=0):
     if 0 <= idx < len(my_lst):
         del my_lst[idx]
     return my_lst
-
-if __name__ == "__main__":
-    lst = [1, 2, 3, 4, 5]
-    newlst = delete_at(lst, -2)
-    print(newlst)
-    new2 = delete_at(lst, 5)
-    print(newlst)
--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -9,11 +9,3 @@ def add_tuple(tuple_a=(), tuple_b=()):
     elif len(tuple_b) == 0:
         tuple_b = (0, 0)
     return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
-
-
-if __name__ == "__main__":
-    tuple_a = (1, 89)
-    tuple_b = (88, 11)
-    print(add_tuple(tuple_a, tuple_b))
-    print(add_tuple(tuple_a, (1,)))
-    print(add_tuple(tuple_a, ()))
