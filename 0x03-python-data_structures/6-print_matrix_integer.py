fc0573162b02d464350854df02aaa15b926165dd
anasyaser <anoos649@gmail.com>
2023-06-10 09:26:20 +0000

    Fix print matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -5,5 +5,3 @@ def print_matrix_integer(matrix=[]):
             for num in row[:-1]:
                 print("{:d}".format(num), end=' ')
             print("{:d}".format(row[-1]))
-    else:
-        print()
