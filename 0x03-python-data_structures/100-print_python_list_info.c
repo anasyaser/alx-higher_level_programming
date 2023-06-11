ccf57d49277401fb6d29f8e6e8f38b927adc4228
anasyaser <anoos649@gmail.com>
2023-06-11 19:40:29 +0300

    python object

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,23 @@
+import <listobject.h>
+import <object.h>
+import <stdio.h>
+import <stdlib.h>
+
+
+void print_python_list_info(PyObject *p)
+{
+	Py_ssize_t len = PyList_size(p);
+
+
+
+
+
+	printf("[*] size of the Python List = %d\n", len);
+	printf("[*] Allocated = %d\n", );
+
+	while (len)
+	{
+		
+	}
+
+}
