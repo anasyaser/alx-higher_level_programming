d3b17fc8ecb1ad4b24519372f055c9fd3c82ac84
anasyaser <anoos649@gmail.com>
2023-06-14 19:53:21 +0000

    Fix square matrix simple

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    result = []
-    for mat in matrix:
-        result.append([x ** 2 for x in mat])
-    return result
+    return [[num ** 2 for num in row] for row in matrix]
