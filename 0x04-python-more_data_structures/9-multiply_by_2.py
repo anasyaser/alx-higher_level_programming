25fccb909d867975c27f3e581db97330cb7468b5
anasyaser <anoos649@gmail.com>
2023-06-14 12:59:50 +0000

    Fix multiply by 2

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 def multiple_by_2(a_dict):
-    return dict([(k, v * 2) for k, v in a_dict.items()])
+    if a_dict:
+        return dict([(k, v * 2) for k, v in a_dict.items()])
