17b5d3d191a2211dd952fec2727597cf45a44058
anasyaser <anoos649@gmail.com>
2023-06-14 12:53:41 +0000

    Fix simple delete

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,15 +1,5 @@
 #!/usr/bin/python3
 def simple_delete(a_dict, key=""):
-    if key in a_dict:
+    if a_dict and key in a_dict:
         del a_dict[key]
     return a_dict
-
-
-if __name__ == "__main__":
-    a = {'language': 'c', 'number': 89, 'track': "low level"}
-
-    new_dict = simple_delete(a, 'track')
-    new_dict = simple_delete(a)
-    print(new_dict)
-    new_dict = simple_delete(a, 'cisfun')
-    print(a)
