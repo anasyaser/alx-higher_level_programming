0e41248029f643a23a5d195c49b4e1fe4c8822d2
anasyaser <anoos649@gmail.com>
2023-06-14 12:48:12 +0000

    Fix print sorted dictionary

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dict):
-    print('\n'.join([f'{key}: {val}' for key, val in sorted(a_dict.items())]))
+    if (a_dict):
+        print('\n'.join([f'{k}: {v}' for k, v in sorted(a_dict.items())]))
+
+
+if __name__ == "__main__":
+    a_dict = {'lang': 1, 'Number': 98, 'track': "low lweve", 'a': {'c': 1, 'b': 2}}
+    print_sorted_dictionary(a_dict)
