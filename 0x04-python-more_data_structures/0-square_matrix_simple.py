11760fe9ece3f3011404c9380ff931a312dcfcc9
anasyaser <anoos649@gmail.com>
2023-06-14 21:00:03 +0000

    return to prev solution

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,9 +1,2 @@
 def square_matrix_simple(matrix=[]):
-    nmatrix = matrix.copy()
-    result = []
-    for lst in matrix:
-        cur_lst = []
-        for num in lst:
-            cur_lst.append(num ** 2)
-        result.append(cur_lst)
-    return result
+    return [[num ** 2 for num in row] for row in matrix]
