719c84cd0953bb17d28cd12718d0a2a9512b6554
anasyaser <anoos649@gmail.com>
2023-06-14 23:21:00 +0300

    Fix roman to integer

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,7 @@
 def roman_to_int(roman_string):
     roman_int_dict = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10,
                       'V': 5, 'I': 1}
-    if not roman_string:
+    if not roman_string or type(roman_string) != str:
         return 0
     result = [0]
     for sym in roman_string:
