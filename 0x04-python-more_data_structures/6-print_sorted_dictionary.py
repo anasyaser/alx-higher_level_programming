93a6d0ef92370ef00f7b3d499b5d4ff3eeffd766
anasyaser <anoos649@gmail.com>
2023-06-14 12:49:00 +0000

    Fix style

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,8 +2,3 @@
 def print_sorted_dictionary(a_dict):
     if (a_dict):
         print('\n'.join([f'{k}: {v}' for k, v in sorted(a_dict.items())]))
-
-
-if __name__ == "__main__":
-    a_dict = {'lang': 1, 'Number': 98, 'track': "low lweve", 'a': {'c': 1, 'b': 2}}
-    print_sorted_dictionary(a_dict)
