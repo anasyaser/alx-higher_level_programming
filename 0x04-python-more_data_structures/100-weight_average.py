d8d1590e5fb5dda345fa8850d5f1c2a88ad624d7
anasyaser <anoos649@gmail.com>
2023-06-14 15:18:13 +0000

    Fix wight average)

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
+    if not my_list:
+        return (0)
     denom = sum([w * v for w, v in my_list])
     numin = sum([v for w, v in my_list])
     return denom / numin
