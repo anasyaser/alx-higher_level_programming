7693ab603c29381fb19b2654dabd48af6c046b6f
anasyaser <anasyaser@gmail.com>
2023-06-26 17:34:16 +0300

    Fix list division

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -13,4 +13,18 @@ def list_division(lst1, lst2, lst_lenght):
             print("wrong type")
         finally:
             pass
-        return result
+    return result
+if __name__ == "__main__":
+    my_l_1 = [10, 8, 4]
+    my_l_2 = [2, 4, 4]
+    result = list_division(my_l_1, my_l_2, max(len(my_l_1),
+                                               len(my_l_2)))
+    print(result)
+
+    print("--")
+
+    my_l_1 = [10, 8, 4, 4]
+    my_l_2 = [2, 0, "H", 2, 7]
+    result = list_division(my_l_1, my_l_2, max(len(my_l_1),
+                                               len(my_l_2)))
+    print(result)
