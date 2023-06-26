4fbd0d96d31d5bc8d658415d11edccf4af260a0b
anasyaser <anasyaser@gmail.com>
2023-06-26 22:14:35 +0300

    Fix list division

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def list_division(lst1, lst2, lst_lenght):
-    result = [0] * max(len(lst1), len(lst2))
-    for i in range(len(result)):
+    result = [0] * lst_lenght
+    for i in range(lst_lenght):
         try:
             result[i] = lst1[i] / lst2[i]
         except ZeroDivisionError:
