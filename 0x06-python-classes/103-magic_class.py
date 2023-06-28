61fb7a9752bb4884af9e2f103c54babed73330aa
anasyaser <anasyaser@gmail.com>
2023-06-28 15:37:11 +0300

    Magic class

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
+"""Creat magic class"""
+
+
 class MagicClass:
+    """
+    Create Magic class
+
+    Attributes:
+        radius: circle radius
+    """
 
     def __init__(self, radius):
         if type(radius) is not int or type(radius) is not float:
@@ -7,7 +16,9 @@ class MagicClass:
         self.__radius = radius
 
     def area(self):
+        """Calculate area of circle"""
         return self.__radius * (math.pi ** 2)
 
     def circumference(self):
+        """Calculate circumference of circle"""
         return 2 * math.pi * self.__radius
