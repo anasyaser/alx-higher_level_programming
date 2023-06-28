7c80f0911d44d97408f8cbc89b41616c63cfdf00
anasyaser <anasyaser@gmail.com>
2023-06-28 08:23:55 +0300

    Fix print position

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -47,7 +47,9 @@ class Square:
         Args:
             value: (tuple) of two positive integers
         """
-        if (len(value) == 2) and value[0] >= 0 and value[1] >= 0:
+        if isinstance(value, tuple) and (len(value) == 2) and\
+           isinstance(value[0], int) and isinstance(value[1], int) and\
+           value[0] >= 0 and value[1] >= 0:
             self.__position = value
         else:
             raise TypeError("position must be a tuple of 2 positive integers")
