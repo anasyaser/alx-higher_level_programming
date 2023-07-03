9ad4fc7ec554886ec9b73aa5fb68bbfdd128590a
anasyaser <anasyaser@gmail.com>
2023-07-04 00:27:21 +0300

    Fix class attribute

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -15,7 +15,7 @@ class Rectangle:
     number_of_instances = 0
 
     def __init__(self, width=0, height=0):
-        number_of_instances += 1
+        type(self).number_of_instances += 1
         self.width = width
         self.height = height
 
@@ -82,5 +82,5 @@ class Rectangle:
         return "Rectangle({}, {})".format(self.width, self.height)
 
     def __del__(self):
-        number_of_instances -= 1
+        type(self).number_of_instances -= 1
         print("Bye rectangle...")
