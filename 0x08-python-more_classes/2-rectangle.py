12d5bd225445bd1165775619386848c3e342d0a0
anasyaser <anasyaser@gmail.com>
2023-07-03 23:17:48 +0300

    Fix typos

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -29,7 +29,7 @@ class Rectangle:
             value: (int) rectangle width
         """
         if type(value) != int:
-            raise TypeError("widht must be an integer")
+            raise TypeError("width must be an integer")
         elif value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
@@ -47,9 +47,9 @@ class Rectangle:
             value: (int) rectangle height
         """
         if type(value) != int:
-            raise TypeError("widht must be an integer")
+            raise TypeError("height must be an integer")
         elif value < 0:
-            raise ValueError("width must be >= 0")
+            raise ValueError("height must be >= 0")
         self.__height = value
 
     def area(self):
