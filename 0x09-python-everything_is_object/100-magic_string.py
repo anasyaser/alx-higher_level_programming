c104daf804ce58b5f60dfb0b4eb1b4d20a34751b
anasyaser <anasyaser@gmail.com>
2023-07-04 14:27:30 +0300

    Lock attributes

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -2,7 +2,3 @@
 def magic_string(lst=[]):
     lst.append("BestSchool")
     return ",".join(lst)
-
-
-for i in range(4):
-    print(magic_string())
---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Create class with only specified attributes
+"""
+
+
+class LockedClass:
+    """
+    class with only specified attributes
+    """
+    __slots__ = {"first_name"}
