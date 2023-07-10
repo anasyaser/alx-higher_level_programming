a2ce07e3d1a7ebf6c13fbf2b987fda68a4a8b895
anasyaser <anasyaser@gmail.com>
2023-07-10 21:04:41 +0300

    Fix typos

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -14,11 +14,3 @@ def inherits_from(obj, a_class):
         True if obj class is inherited from a_class else False
     """
     return isinstance(obj, a_class) and type(obj).__name__ != a_class.__name__
-
-a = True
-if inherits_from(a, int):
-    print("{} inherited from class {}".format(a, int.__name__))
-if inherits_from(a, bool):
-    print("{} inherited from class {}".format(a, bool.__name__))
-if inherits_from(a, object):
-    print("{} inherited from class {}".format(a, object.__name__))
