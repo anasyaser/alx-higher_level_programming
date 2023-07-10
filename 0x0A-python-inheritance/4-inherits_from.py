6b4a1b69647894e83ad11d00b5922465077d10db
anasyaser <anasyaser@gmail.com>
2023-07-10 21:04:19 +0300

    Fix inherited class

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -4,13 +4,21 @@
 
 def inherits_from(obj, a_class):
     """
-    Check if obect is inherited from class
+    Check if obect class is inherited from class
 
     Args:
         obj: boject
         a_class: class
 
     Return:
-        True if obj is inherited from a_class else False
+        True if obj class is inherited from a_class else False
     """
-    return isinstance(obj, a_class)
+    return isinstance(obj, a_class) and type(obj).__name__ != a_class.__name__
+
+a = True
+if inherits_from(a, int):
+    print("{} inherited from class {}".format(a, int.__name__))
+if inherits_from(a, bool):
+    print("{} inherited from class {}".format(a, bool.__name__))
+if inherits_from(a, object):
+    print("{} inherited from class {}".format(a, object.__name__))
