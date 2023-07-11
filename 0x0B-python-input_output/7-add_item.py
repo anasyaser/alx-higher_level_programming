4cb59fa3a91086301e0b9cb9db4302d1b4ced980
anasyaser <anasyaser@gmail.com>
2023-07-11 23:59:08 +0300

    Fix style

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -8,11 +8,12 @@ load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
 
 def add_item(file_name, args):
-        try:
-            file_lst = load_from_json_file(file_name)
-            file_lst += args
-            save_to_json_file(file_lst, file_name)
-        except:
-            save_to_json_file(args, file_name)
+    try:
+        file_lst = load_from_json_file(file_name)
+        file_lst += args
+        save_to_json_file(file_lst, file_name)
+    except e:
+        save_to_json_file(args, file_name)
+
 
 add_item("add_item.json", sys.argv[1:])
