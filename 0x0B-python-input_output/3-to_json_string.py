34e9c0f78f0d649ff8224931a82579211d3b407f
anasyaser <anasyaser@gmail.com>
2023-07-11 23:12:43 +0300

    Convert to json Sting

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+Convert python data structure to json object
+"""
+import json
+
+def to_json_string(my_obj):
+    """Convert Python object to json"""
+    return json.dumps(my_obj)
