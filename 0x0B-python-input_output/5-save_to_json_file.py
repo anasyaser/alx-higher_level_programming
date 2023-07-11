96c364c2696c82d5c8b99df7eb47596bcbb55851
anasyaser <anasyaser@gmail.com>
2023-07-11 23:27:01 +0300

    Save to json file

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Convert python data structure to json object and save it to file
+"""
+import json
+
+
+def save_to_json_string(my_obj, file_name):
+    """Convert Python object to json and save it to file"""
+    with open(file_name, "w", encoding="utf-8") as f:
+        return json.dump(my_obj, f)
