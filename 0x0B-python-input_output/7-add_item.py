f0a5e8fd559655fb9bac0ce3c2f535847789c171
anasyaser <anasyaser@gmail.com>
2023-07-12 14:17:49 +0300

    Fix add item

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -8,14 +8,14 @@ load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
 
 def add_item(file_name, args):
-    """Add all script arguments to list and save them as json file object"""
+    """Add script arguments to list and save them as json file objectooooj"""
     try:
         file_lst = load_from_json_file(file_name)
         file_lst += args
         save_to_json_file(file_lst, file_name)
-    except e:
+    except Exception:
         save_to_json_file(args, file_name)
 
-if __name__ == "__main__":
 
+if __name__ == "__main__":
     add_item("add_item.json", sys.argv[1:])
