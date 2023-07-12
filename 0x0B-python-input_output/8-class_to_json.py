548967c835e9385c52a8ad240ad7b50c43b7ffc0
anasyaser <anasyaser@gmail.com>
2023-07-12 09:07:54 +0300

    Convert object instanse attributes to json object

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Convert class to json object
+"""
+import json
+
+
+def class_to_json(obj):
+    """Convert calss attributes to python dictionary"""
+    return json.dumps(vars(obj))
