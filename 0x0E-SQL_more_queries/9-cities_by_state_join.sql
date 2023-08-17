fbb3df19c56aebccacaa4fd976dfd91462d52b9d
anasyaser <anoos649@gmail.com>
2023-08-17 15:53:31 +0300

    Task 9: Join tables

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,7 @@
+-- Using subquery to filter
+SELECT cities.id, cities.name, states.name
+       FROM cities
+       INNER JOIN states
+       ON cities.state_id = states.id
+       ORDER BY cities.id;
+
