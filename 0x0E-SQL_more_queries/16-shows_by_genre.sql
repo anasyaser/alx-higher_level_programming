14fe4c0a9b1d14573827948be6c650132776dae8
anasyaser <anoos649@gmail.com>
2023-08-17 22:22:48 +0300

    Fix show by genre

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -4,6 +4,6 @@ SELECT tv_shows.title AS title,
 FROM tv_shows
 LEFT JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.show_id
-INNER JOIN tv_genres
+LEFT JOIN tv_genres
 ON tv_genres.id = tv_show_genres.genre_id
 ORDER BY title;
