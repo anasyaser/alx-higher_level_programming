1ad4bc1cf0e93bcf14591fba46b26f7f2f4ee5c7
anasyaser <anoos649@gmail.com>
2023-08-17 11:10:54 +0300

    Fix some type issues and task 3

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,4 @@
 -- Create DATABASE and USER with custome privileges
-CREATE DATABASE IF NOT EXIST hbtn_0d_2;
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT on *.* TO 'user_0d_1@localhost';
+GRANT SELECT on hbtn_0d_2.* TO 'user_0d_2'@'localhost';
