6f53a258b71f8fc1f215d4e2a3a56619ac8ec07a
anasyaser <anoos649@gmail.com>
2023-08-17 12:12:29 +0300

    Using subquery

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,10 @@
+-- Using subquery to filter
+SELECT id, name
+       FROM cities
+       WHERE state_id =
+       (SELECT id
+         FROM states
+         WHERE name = 'California');
+
+       
+
