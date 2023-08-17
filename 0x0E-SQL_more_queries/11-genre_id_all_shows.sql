df298dabecd94700fcc2fd60d81a9492dfdb38e1
anasyaser <anoos649@gmail.com>
2023-08-17 18:54:22 +0300

    Task 11: Show all shows

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,7 @@
+-- Show all shows although if it is not have gnere
+-- List all shows that has linked genre
+SELECT tv_shows.title, tv_show_genres.genre_id
+       FROM tv_shows
+       LEFT JOIN tv_show_genres
+       ON tv_shows.id = tv_show_genres.show_id
+       ORDER BY tv_shows.title, tv_show_genres.genre_id;
