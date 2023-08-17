d3b3221d0c053534b93ef43addfd65b5780f8a1c
anasyaser <anoos649@gmail.com>
2023-08-17 22:21:18 +0300

    Task 16: show by genre

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,9 @@
+-- show shows by genre
+SELECT tv_shows.title AS title,
+       tv_genres.name AS name
+FROM tv_shows
+LEFT JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+INNER JOIN tv_genres
+ON tv_genres.id = tv_show_genres.genre_id
+ORDER BY title;
