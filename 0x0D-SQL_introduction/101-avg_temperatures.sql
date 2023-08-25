e484b643fe87172502f736cf152da6f4ca56c4ea
anasyaser <anoos649@gmail.com>
2023-08-25 11:46:52 +0300

    Fix typo issue

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,5 +1,5 @@
 -- Calculate average temperatures for each city
 SELECT city, avg(value) AS avg_temp
-       FROM hbtn_0c_0
+       FROM temperatures
        GROUP BY city
-       ORDER BY avg_tmp DESC;
+       ORDER BY avg_temp DESC;
