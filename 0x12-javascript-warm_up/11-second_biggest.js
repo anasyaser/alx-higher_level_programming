42ff8673ca28ef1402188f39148c924cbeb28d5c
anasyaser <anoos649@gmail.com>
2023-09-13 00:40:52 +0300

    Fix secong maximum number

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,24 +1,8 @@
 #!/usr/bin/node
 const args = process.argv;
-const sliced = args.slice(2).map(Number);
-function getMax (arr) {
-  let mx = arr[0];
-  for (let i = 1; i < arr.length; i++) {
-    if (arr[i] > mx) {
-      mx = arr[i];
-    }
-  }
-  return mx;
-}
+const sliced = args.slice(2).map(Number).sort((a, b) => b - a);
 if (args[2] && args.length > 3) {
-  const maxNum = getMax(sliced);
-  let curr = sliced[0];
-  for (let i = 0; i < sliced.length; i++) {
-    if (sliced[i] > curr && sliced[i] < maxNum) {
-      curr = sliced[i];
-    }
-  }
-  console.log(curr);
+  console.log(sliced[1]);
 } else {
   console.log(0);
 }
