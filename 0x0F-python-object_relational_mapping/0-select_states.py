3380b001e836bd4d0f6f9b97ab01783f80d078cc
anasyaser <anoos649@gmail.com>
2023-09-14 22:43:25 +0300

    Intialize files

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Connect python to MySQLdb module and write mysql query"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    args = sys.argv[1:]
+    conn = MySQLdb.connect(host="localhost", port=3306, user=args[0],
+                           passwd=args[1], db=args[2], charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
