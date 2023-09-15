d53530aaff4a4ffa0075e931a0b6ff62659cf93a
anasyaser <anoos649@gmail.com>
2023-09-15 11:29:33 +0300

    All cities

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Connect python to MySQLdb module and fetch all cities
+"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    args = sys.argv[1:]
+    conn = MySQLdb.connect(host="localhost", port=3306, user=args[0],
+                           passwd=args[1], db=args[2])
+    cur = conn.cursor()
+    query = "SELECT cities.id, cities.name, states.name FROM cities
+    LEFT JOIN states ON cities.state_id = states.id ORDER BY cities.id"
+    cur.execute(query)
+
+    query_rows = cur.fetchall()
+
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
