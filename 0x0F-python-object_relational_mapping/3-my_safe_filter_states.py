46c34f17240b46f510abfc2549e642d97dfccd73
anasyaser <anoos649@gmail.com>
2023-09-17 23:23:42 +0300

    My safe filter states

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Connect python to MySQLdb module and fetch all states match given name
+BUT make sure to be safe from  sql injection attacks
+"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    args = sys.argv[1:]
+    conn = MySQLdb.connect(host="localhost", port=3306, user=args[0],
+                           passwd=args[1], db=args[2])
+    cur = conn.cursor()
+    query = "SELECT * FROM states WHERE binary name = %s"
+    cur.execute(query, args[3])
+
+    query_rows = cur.fetchall()
+
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
