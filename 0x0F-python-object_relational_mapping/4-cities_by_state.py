bd5499b744468737653be029c3f76c9db97276ba
anasyaser <anoos649@gmail.com>
2023-09-17 10:23:57 +0300

    Try to fix task4

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""
-Connect python to MySQLdb module and fetch all cities
-"""
+""" Connect python to MySQLdb module and fetch all cities """
 import MySQLdb
 import sys
 
@@ -10,8 +8,10 @@ if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost", port=3306, user=args[0],
                            passwd=args[1], db=args[2])
     cur = conn.cursor()
-    query = "SELECT cities.id, cities.name, states.name FROM cities
-    LEFT JOIN states ON cities.state_id = states.id ORDER BY cities.id"
+    query = "SELECT cities.id, cities.name, states.name FROM cities\
+    INNER JOIN states\
+    ON cities.state_id = states.id\
+    ORDER BY cities.id"
     cur.execute(query)
 
     query_rows = cur.fetchall()
