e88154a7ef5a55968947cf9b3b9e534974f23344
anasyaser <anoos649@gmail.com>
2023-09-20 00:10:11 +0300

    Fix typo

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -12,11 +12,14 @@ Base = declarative_base()
 
 
 class City(Base):
-    """Declartive Calss to City Table"""
+    """
+    Declartive Calss to City Table
+    Attribute:
+    id, name, state_id, state
+    """
     __tablename__ = 'cities'
 
     id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, nullable=False, ForeignKey('states.id'))
-
-    state = relationship("State", back_populates="cities")
+    
