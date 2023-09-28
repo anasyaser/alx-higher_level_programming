7b75a785450291ae1af85e18e1752460d9e43ef6
anasyaser <anoos649@gmail.com>
2023-09-28 21:30:21 +0300

    Fix task 2

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Display the body of 200 response
-if [ "$(curl -sIL "$1" | head -1 | cut -d ' ' -f 2)" = '200' ]; then curl -s "$1"; fi
+if [ "$(curl -sIL "$1" -X GET | head -1 | cut -d ' ' -f 2)" = '200' ]; then curl -s "$1"; fi
