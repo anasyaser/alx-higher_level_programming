ab0a3e5b82006c8524ce4f6115ab19b446c2a24e
anasyaser <anoos649@gmail.com>
2023-09-29 23:40:13 +0300

    Task 8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" JSON API """
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        v = ""
+    v = sys.argv[1]
+    r = requests.post('http://0.0.0.0:5000/search_user', data={'q': v})
+    try:
+        j = r.json()
+        if j:
+            print("[{}] {}".format(j['id'], j['name']))
+        else:
+            print('No result')
+    except Exception as e:
+        print("Not a valid JSON")
