b9809b2ea0683399c654f29b4afba96cac7d0167
anasyaser <anoos649@gmail.com>
2023-10-24 20:14:52 +0300

    READ File

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,12 +4,18 @@ Find the peak of list
 """
 def find_peak(lst):
     """Find the peak of list"""
-    idx = len(lst) // 2
-    if lst[idx+1] and lst[idx+1] > lst[idx]:
+    length = len(lst)
+    idx = length // 2
+    if (length == 0):
+        return
+    if (length == 1):
+        return lst[0]
+    if (idx + 1 < length) and lst[idx+1] > lst[idx]:
         return find_peak(lst[idx+1:])
-    elif lst[idx-1] and lst[idx-1] > lst[idx]:
+    elif (idx - 1 > -1) and lst[idx-1] > lst[idx]:
         return find_peak(lst[:idx])
-    elif lst[idx-1] andlst[idx] == lst[idx-1] == lst[idx+1]:
-        return max(find_peak(lst[:idx], lst[idx+1:]))
+    elif (idx - 1 > -1) and (idx + 1 < length) and\
+         (lst[idx] == lst[idx-1] == lst[idx+1]):
+        return max(find_peak(lst[:idx]), find_peak(lst[idx+1:]))
     else:
         return lst[idx]
