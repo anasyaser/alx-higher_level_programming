a511b35f4d289d386cca894a6cf7452a8c662953
anasyaser <anoos649@gmail.com>
2023-10-24 20:41:01 +0300

    Fix status code

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,6 +1,8 @@
 #!/usr/bin/node
 const request = require('request');
 request(process.argv[2], function (error, response, body) {
-  console.error('error:', error);
+  if (error) {
+    console.log(error);
+  }
   console.log('code:', response.statusCode);
 });
