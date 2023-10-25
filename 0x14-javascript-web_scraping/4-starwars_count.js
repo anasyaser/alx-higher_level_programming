6beed01edae3d047e2b5f1bc63f1c2bf9e95b0eb
anasyaser <anoos649@gmail.com>
2023-10-25 06:53:10 +0300

    Fix starwars count

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,16 +1,19 @@
 #!/usr/bin/node
 const request = require('request');
 const episodesURL = process.argv[2];
-const charURL = 'https://swapi-api.alx-tools.com/api/people/18/';
 request(episodesURL, function (error, response, body) {
   if (error) {
     console.log(error);
   }
-  const json = JSON.parse(response.body);
+    const json = JSON.parse(response.body);
+    const results = json.results;
   let count = 0;
-  for (let i = 0; i < json.results.length; i++) {
-    if (json.results[i].characters.includes(charURL)) {
-      count += 1;
+    for (let i = 0; i < results.length; i++) {
+      let characters = results[i].characters
+      for (let j = 0; j < characters.length; j++) {
+	  if (characters[j].includes('/18/')) {
+            count += 1;
+	  }
     }
   }
   console.log(count);
