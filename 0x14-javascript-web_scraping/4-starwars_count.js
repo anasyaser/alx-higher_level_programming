02acdb4ddb01fda55502420d9c5a214e56528244
anasyaser <anoos649@gmail.com>
2023-10-25 12:14:06 +0300

    Fix starwars count

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -5,15 +5,15 @@ request(episodesURL, function (error, response, body) {
   if (error) {
     console.log(error);
   }
-    const json = JSON.parse(response.body);
-    const results = json.results;
+  const json = JSON.parse(response.body);
+  const results = json.results;
   let count = 0;
-    for (let i = 0; i < results.length; i++) {
-      let characters = results[i].characters
-      for (let j = 0; j < characters.length; j++) {
-	  if (characters[j].includes('/18/')) {
-            count += 1;
-	  }
+  for (let i = 0; i < results.length; i++) {
+    const characters = results[i].characters;
+    for (let j = 0; j < characters.length; j++) {
+      if (characters[j].includes('/18/')) {
+        count += 1;
+      }
     }
   }
   console.log(count);
