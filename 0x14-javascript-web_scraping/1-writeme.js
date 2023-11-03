65aca11e9de417f078c88799ed190e6c80b8d88b
anasyaser <anoos649@gmail.com>
2023-11-03 08:00:00 +0200

    Deferment to another cohort

--- 0x14-javascript-web_scraping/1-writeme.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/node
-const fs = require('fs');
-const filePath = process.argv[2];
-fs.writeFile(filePath, process.argv[3], function (err) {
-  if (err) {
-    console.log(err);
-  }
-});
